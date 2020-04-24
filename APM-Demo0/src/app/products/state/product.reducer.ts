4e4b6ca98f64167d885960666e0a9b1f2b5c4b2d
Ammar <ammar4568@gmail.com>
2020-04-24 22:48:11 +0500

    Subscribing to the Store to Get State Changes

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,6 +1,4 @@
 export function reducer(state, action) {
-    console.log('existing state', JSON.stringify(state))
-    console.log('payload', action.payload)
     switch (action.type) {
         case 'TOGGLE_PRODUCT_CODE':
             return {
