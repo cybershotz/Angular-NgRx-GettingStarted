84c758ace9787af836b13e8a5a4ef77a58a1b443
Ammar <ammar4568@gmail.com>
2020-04-24 22:41:55 +0500

    Dispatching an Action to Change State

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,4 +1,6 @@
 export function reducer(state, action) {
+    console.log('existing state', JSON.stringify(state))
+    console.log('payload', action.payload)
     switch (action.type) {
         case 'TOGGLE_PRODUCT_CODE':
             return {
